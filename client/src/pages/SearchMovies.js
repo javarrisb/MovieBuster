305d2dc7cf336bdb68a1ca5c26caee525e1617d6
Javarrisb <javarrisb@gmail.com>
2023-01-12 00:03:12 -0500

    search movies

--- client/src/pages/SearchMovies.js
+++ client/src/pages/SearchMovies.js
@@ -74,4 +74,69 @@ const SearchMovies = () => {
             console.error(err);
         }
     };
-}
+
+    return (
+        <>
+        <Jumbotron fluid className='text-light bg-dark'>
+            <Container>
+                <h1>Search Movies here!</h1>
+                <Form onSubmit={handleFormSubmit}>
+                    <Form.Row>
+                        <Col xs={12} md={8}>
+                            <Form.Control
+                            name='searchInput'
+                            value={searchInput}
+                            onChange={(e) => setSearchInput(e.target.value)}
+                            type='text'
+                            size='lg'
+                            placeholder='Search for a movie'
+                            />
+                            </Col>
+                            <Col xs={12} md={4}>
+                                <Button type='submit' variant='success' size='lg'>
+                                    Submit Search
+                                    </Button>
+                                    </Col>
+                                    </Form.Row>
+                                    </Form>
+                                    </Container>
+                                    </Jumbotron>
+
+                                    <Container>
+                                        <h2>
+                                           {searchedMovies.length
+                                           ?  `Seeing ${searchedMovies.length} results:`
+                                        : 'Search for a movie to start'} 
+                                        </h2>
+                                        <CardColumns>
+                                            {searchedMovies.map((movie) => {
+                                             return (
+                                                <Card key={movie.movieId} border='light'>
+                                                    {movie.poster_path ? (
+                                                        <Card.Img src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={`Poster for ${movie.original_title}`} variant='top' />
+                                                    ) :null}
+                                                    <Card.Body>
+                                                        <p className='small'>Release Date: {movie.release_date}</p>
+                                                        <p className='small'>Rating: {movie.rating} /10</p>
+                                                        <p className='small'>Tagline: {movie.tagline}</p>
+                                                        {Auth.loggedIn() && (
+                                                            <Button 
+                                                            disabled={savedMovieIds?.some((savedMovieId) => savedMovieId === movie.movieId)}
+                                                            className='btn-block btn-info'
+                                                            onClick={() => handleSaveMovie(movie.movieId)}>
+                                                                {savedMovieIds?.some((savedMovieId) => savedMovieId === movie.movieId)
+                                                                ? 'This movie has been saved already!'
+                                                            : 'Save this Movie!'}
+                                                            </Button>
+                                                        )}
+                                                    </Card.Body>
+                                                </Card>
+                                             );   
+                                            })}
+                                        </CardColumns>
+                                    </Container>
+            </>
+    );
+};
+
+export default SearchMovies;
